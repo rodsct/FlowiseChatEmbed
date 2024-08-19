959d33b5aef7b8f8296bc13a23c74e12d409c0e3
Rodrigo <75404646+rodsct@users.noreply.github.com>
2024-08-19 00:05:17 -0600

    ok

--- src/components/Bot.tsx
+++ src/components/Bot.tsx
@@ -125,7 +125,7 @@ export type LeadsConfig = {
   successMessage?: string;
 };
 
-const defaultWelcomeMessage = 'Hi there! How can I help?';
+const defaultWelcomeMessage = 'Hola, ¿como te puedo ayudar?';
 
 /*const sourceDocuments = [
     {
